72fae39f0f8ca51b5fa748c101fc650239c48cef
Enclaive-lab <aap.porno228.iorio@gmail.com>
2026-02-05 23:59:50 +0300

    Add files via upload

---
+++ variables-js/main.js
@@ -0,0 +1,34 @@
+const firstName = "Егор";
+const lastName = "Терехин";
+const isStudent = `:
+true`;
+
+const age = 22;
+
+const currentYear = 2025;
+
+const sum = 2025 - 22;
+
+const birthYear = 2003;
+
+console.log(birthYear);
+
+console.log(
+  "Меня зовут",
+  firstName,
+  lastName,
+  ",",
+  "мне",
+  age,
+  "года.",
+  "Я ученик курса",
+  isStudent,
+);
+
+let a = "123";
+let b = +"456";
+let c = Number("789");
+let d = Boolean(0);
+let e = Boolean(" ");
+
+console.log(a + b + c + d + e);
