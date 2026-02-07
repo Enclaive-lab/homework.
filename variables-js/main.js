8578944f4a61063e950107d72d3f19ce1deac668
Enclaive-lab <aap.porno228.iorio@gmail.com>
2026-02-07 23:03:27 +0300

    Add files via upload

--- variables-js/main.js
+++ variables-js/main.js
@@ -1,34 +1,72 @@
-const firstName = "Егор";
-const lastName = "Терехин";
-const isStudent = `:
-true`;
+// 1 задача
 
-const age = 22;
+const number1 = "4";
 
-const currentYear = 2025;
+if (number1 === "4" || number1 === "6" || number1 === "8" || number1 === "10") {
+  console.log("корректные данные");
+} else {
+  console.log("некорректные данные");
+}
 
-const sum = 2025 - 22;
+// 2 задача
 
-const birthYear = 2003;
+const age = "16";
+const discount = "10";
+const age3 = "67";
+const age2 = "64";
 
-console.log(birthYear);
+const result = age < 18 ? "скидка 10" : "скидка не действует ";
+console.log(result);
 
-console.log(
-  "Меня зовут",
-  firstName,
-  lastName,
-  ",",
-  "мне",
-  age,
-  "года.",
-  "Я ученик курса",
-  isStudent,
-);
+const result1 = age2 >= 18 && age2 <= 65 ? "скидка 20" : "скидка не действует";
+console.log(result1);
 
-let a = "123";
-let b = +"456";
-let c = Number("789");
-let d = Boolean(0);
-let e = Boolean(" ");
+const result2 = age3 > 65 ? "скидка 30" : "скидка не действует ";
+console.log(result2);
 
-console.log(a + b + c + d + e);
+// доп 2 задача
+
+switch (age) {
+  case age < 18 ? "скидка 10" : "скидка не действует ":
+    console.log(result);
+    break;
+}
+switch (age2) {
+  case age2 >= 18 && age2 <= 65 ? "скидка 20" : "скидка не действует":
+    console.log(result1);
+    break;
+}
+
+switch (age3) {
+  case age3 > 65 ? "скидка 30" : "скидка не действует ":
+    console.log;
+    console.log(result2);
+    break;
+}
+
+// 3 задача
+
+const username = "admin";
+const password = "123456";
+
+const a = prompt("Введите данные имя пользователя");
+const b = prompt("Введите данные пароля пользователя");
+if (a === username && b === password) {
+  console.log("Доступ разрешен");
+} else {
+  console.log("Доступ запрещен");
+}
+
+// 4 задача
+
+const standard = "Стандарт";
+const express = "Экспресс";
+const premium = "Премиум";
+const weight = "25";
+
+let delivery = standard;
+
+switch (weight) {
+  case weight <= 0 ? "Некорректный вес посылки" : "Подходящий вес ":
+    console.log("text");
+}
