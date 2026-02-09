771a61b39ef199e23150b6f9a32c0147257b50d7
Enclaive-lab <aap.porno228.iorio@gmail.com>
2026-02-10 00:35:40 +0300

    Add files via upload

---
+++ cycles-js/main.js
@@ -0,0 +1,29 @@
+// Задача 1
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 4 === 0) {
+    console.log(i);
+  }
+}
+
+// задача 2
+
+const number = +prompt("Получение факториала числа", 5);
+
+let factorial = 1;
+
+for (let i = 1; i <= number; i++) {
+  factorial = factorial * i;
+}
+
+console.log(factorial);
+
+// задача 3
+
+// let board = "";
+
+// for (){
+//   let row = "";
+//   for (){
+//    row += "Ч ";
+//   }
