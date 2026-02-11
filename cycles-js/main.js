35abe5ec6a0484527e0c02c70cea809e2979ea9b
Enclaive-lab <aap.porno228.iorio@gmail.com>
2026-02-11 23:19:19 +0300

    Add files via upload

--- cycles-js/main.js
+++ cycles-js/main.js
@@ -1,29 +1,62 @@
-// Задача 1
+// const getFactorial = (num) => {
+//   let factorial = 1;
+//   for (let i = 1; i <= num; i++) {
+//     factorial *= i;
+//   }
+//   return factorial;
+// };
 
-for (let i = 1; i <= 20; i++) {
-  if (i % 4 === 0) {
-    console.log(i);
-  }
+// console.log(getFactorial(3));
+// console.log(getFactorial(2));
+// console.log(getFactorial(6));
+
+// const factorial = getFactorial(5);
+// const isOdd = (num) => {
+//   return num % 2 !== 0;
+// };
+
+// задача 1
+function calculateFinalPrice(Price, discount, tax) {
+  const priceAfterDiscount = Price - (Price * discount) / 100;
+  const finalPrice = priceAfterDiscount + priceAfterDiscount * tax;
+  return finalPrice;
 }
+
+console.log(calculateFinalPrice(100, 10, 0.2));
+console.log(calculateFinalPrice(100, 10, 0));
 
 // задача 2
 
-const number = +prompt("Получение факториала числа", 5);
-
-let factorial = 1;
-
-for (let i = 1; i <= number; i++) {
-  factorial = factorial * i;
+function checkAccess(username, password) {
+  if (username === "admin" && password === "123456") {
+    return "Доступ разрешен";
+  }
+  return "Доступ запрещен";
 }
 
-console.log(factorial);
+console.log(checkAccess("admin", "123456"));
 
-// задача 3
+// Задача 3
 
-// let board = "";
+function getTimeOfDay(hour) {
+  if (hour >= 0 && hour <= 5) return "Ночь";
+  if (hour >= 6 && hour <= 11) return "Утро";
+  if (hour >= 12 && hour <= 17) return "День";
+  if (hour >= 18 && hour <= 23) return "Вечер";
+  return "некорректное время";
+}
+console.log(getTimeOfDay(10));
 
-// for (){
-//   let row = "";
-//   for (){
-//    row += "Ч ";
-//   }
+// задача 4
+
+function findFirstEven(start, end) {
+  for (let i = start; i <= end; i++) {
+    if (i % 2 === 0) {
+      return i;
+    }
+  }
+  return "Четных чисел нет ";
+}
+
+console.log(findFirstEven(1, 10));
+console.log(findFirstEven(9, 9));
