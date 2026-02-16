3ccf6f66676fd61e69b6caa9adc8383d236b19d4
Enclaive-lab <aap.porno228.iorio@gmail.com>
2026-02-16 17:15:36 +0300

    Add files via upload

--- cycles-js/main.js
+++ cycles-js/main.js
@@ -1,79 +1,156 @@
-"use strict";
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// a can contain numbers or strings. x can be either.
+
+// Return true if the array contains the value, false if not.
+
 // задача 1
-const users = [
-  { name: "Alex", age: 24, isAdmin: false },
-  { name: "Bob", age: 13, isAdmin: false },
-  { name: "John", age: 31, isAdmin: true },
-  { name: "Jane", age: 20, isAdmin: false },
-];
 
-const lastUsers = [
-  { name: "Ann", age: 19, isAdmin: false },
-  { name: "Jack", age: 43, isAdmin: true },
-];
-
-users.push(...lastUsers);
-
-console.log(users);
+function check(a, x) {
+  return a.includes(x);
+}
 
 // задача 2
 
-const user = [
-  { name: "Alex", age: 24, isAdmin: false },
-  { name: "Bob", age: 13, isAdmin: false },
-  { name: "John", age: 31, isAdmin: true },
-  { name: "Jane", age: 20, isAdmin: false },
-];
+// Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?".
 
-function getUserAverageAge() {
-  let sum = 0;
-  for (let i = 0; i < user.length; i++) {
-    sum += user[i].age;
-  }
-  return sum / user.length;
+function greet(name) {
+  return `Hello, ${name} how are you doing today?`;
 }
-console.log(getUserAverageAge(user));
 
-// Задача 3
-const use = [
-  { name: "Alex", age: 24, isAdmin: false },
-  { name: "Bob", age: 13, isAdmin: false },
-  { name: "John", age: 31, isAdmin: true },
-  { name: "Jane", age: 20, isAdmin: false },
-];
+// задача 3
 
-const admins = (use) => {
-  const admins = [];
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
-  for (let i = 0; i < use.length; i++) {
-    if (use[i].isAdmin === true) {
-      admins.push(use[i]);
-    }
+function simpleMultiplication(number) {
+  if (number % 2 === 0) {
+    return number * 8;
+  } else {
+    return number * 9;
   }
-
-  return admins;
-};
-
-console.log(admins(use));
+}
 
 // задача 4
 
-function first(arr, n) {
-  if (n === undefined) {
-    return [arr[0]];
-  }
+// A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
 
-  if (n === 0) {
-    return [];
-  }
+// Return true if yes, false otherwise :)
 
-  const result = [];
-  for (let i = 0; i < n; i++) {
-    result.push(arr[i]);
-  }
-  return result;
+function hero(bullets, dragons) {
+  return bullets >= dragons * 2;
 }
 
-console.log(first([1, 2, 3, 4, 5], 3));
-console.log(first([1, 2, 3, 4, 5]));
-console.log(first([1, 2, 3, 4, 5], 0));
+// задача 5
+
+// Task
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example
+// [1, -4, 7, 12] =>
+// 1
+// +
+// 7
+// +
+// 12
+// =
+// 20
+// 1+7+12=20
+// Note
+// // If there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+  return arr.filter((num) => num > 0).reduce((sum, num) => sum + num, 0);
+}
+
+// задача 6
+
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case "+":
+      return value1 + value2;
+    case "-":
+      return value1 - value2;
+    case "*":
+      return value1 * value2;
+    case "/":
+      return value1 / value2;
+  }
+  return 0;
+}
+
+// задача 7
+
+// If you can't sleep, just count sheeps!!
+
+var countSheep = function (num) {
+  let result = "";
+  for (let i = 1; i <= num; i++) {
+    result += `${i} sheep...`;
+  }
+  return result;
+};
+
+// задача 8
+
+// Our football team has finished the championship.
+
+// Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Points are awarded for each match as follows:
+
+// if x > y: 3 points (win)
+// if x < y: 0 points (loss)
+// if x = y: 1 point (tie)
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+// Notes:
+
+// our team always plays 10 matches in the championship
+// 0 <= x <= 4
+// // 0 <= y <= 4
+
+function points(games) {
+  let totalPoint = 0;
+
+  for (i = 0; i < games.length; i++) {
+    let scores = games[i].split(":");
+    let x = Number(scores[0]);
+    let y = Number(scores[1]);
+    if (x > y) {
+      totalPoint += 3;
+    } else if (x === y) {
+      totalPoint += 1;
+    }
+    if (x < y) {
+      totalPoint += 0;
+    }
+  }
+  return totalPoint;
+}
+
+// задача 9
+
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+function doubleChar(str) {
+  return str
+    .split("")
+    .map((char) => char + char)
+    .join("");
+}
+
+// задача 10
+
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+  return s.split("!").join("");
+  return "";
+}
